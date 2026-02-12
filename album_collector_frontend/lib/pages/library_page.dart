c3e8914ceae883e4734e0fca4edc71e540da8df7
Rancid-coder <redflagblackbones47@gmail.com>
2026-02-12 04:38:52 +0000

    feat: Add library page with placeholder text

---
+++ album_collector_frontend/lib/pages/library_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LibraryPage extends StatefulWidget {
+  const LibraryPage({super.key});
+
+  @override
+  State<LibraryPage> createState() => _LibraryPageState();
+}
+
+class _LibraryPageState extends State<LibraryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text(
+              'This is an upcoming feature. You will be able to see all your albums here.')
+        ],
+      ),
+    );
+  }
+}
