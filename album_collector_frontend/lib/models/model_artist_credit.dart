ccd9c6081ba191e81a723fa8050183ad1f479820
Rancid-coder <redflagblackbones47@gmail.com>
2026-02-12 04:16:20 +0000

    feat: Add joinphrase field to ArtistCredit model

--- album_collector_frontend/lib/models/model_artist_credit.dart
+++ album_collector_frontend/lib/models/model_artist_credit.dart
@@ -3,23 +3,26 @@ import 'package:album_collector_frontend/models/model_artist.dart';
 class ModelArtistCredit {
   final String name;
   final ModelArtist artist;
+  final String? joinphrase;
 
   ModelArtistCredit({
     required this.name,
     required this.artist,
+    this.joinphrase,
   });
 
   factory ModelArtistCredit.fromJson(Map<String, dynamic> json) {
     return ModelArtistCredit(
-      name: json['name'],
-      artist: ModelArtist.fromJson(json['artist']),
-    );
+        name: json['name'],
+        artist: ModelArtist.fromJson(json['artist']),
+        joinphrase: json['joinphrase']);
   }
 
   Map<String, dynamic> toJson() {
     return {
       'name': name,
       'artist': artist.toJson(),
+      'joinphrase': joinphrase,
     };
   }
 }
